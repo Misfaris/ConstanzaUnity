3fac44ee3edac2746ac0eaee345817a583cdeedb
Misfaris <constanzarigazio@hotmail.com>
2025-10-06 22:57:14 +0100

    Input from Keyboard
    Input from Keyboard forward/backwards and rotation

--- Prototype 1/Assets/PlayerController.cs
+++ Prototype 1/Assets/PlayerController.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float turnSpeed = 1f;
+    public float horizontalInput;
+    public float forwardInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,7 +17,12 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //We'll make the vehicle move forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-                
+        transform.Translate(Vector3.forward * Time.deltaTime * speed *forwardInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+
+        //Gather input from keyboard
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
     }
 }
